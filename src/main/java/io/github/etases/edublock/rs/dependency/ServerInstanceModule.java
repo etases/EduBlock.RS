0485ef79c14f95aa04bc2bc16e40ade954b21905
HSGamer <huynhqtienvtag@gmail.com>
2022-09-09 01:37:37 +0700

    comment

--- src/main/java/io/github/etases/edublock/rs/dependency/ServerInstanceModule.java
+++ src/main/java/io/github/etases/edublock/rs/dependency/ServerInstanceModule.java
@@ -4,6 +4,9 @@ import com.google.inject.AbstractModule;
 import io.github.etases.edublock.rs.RequestServer;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * A module for Guice to provide the {@link RequestServer}
+ */
 @RequiredArgsConstructor
 public class ServerInstanceModule extends AbstractModule {
     private final RequestServer requestServer;
