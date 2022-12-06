2107a9d3d93570b20b0c05e8e95e68b797ec4773
HSGamer <huynhqtienvtag@gmail.com>
2022-12-06 12:50:56 +0700

    unaccent username

--- src/main/java/io/github/etases/edublock/rs/internal/account/AccountUtil.java
+++ src/main/java/io/github/etases/edublock/rs/internal/account/AccountUtil.java
@@ -3,8 +3,11 @@ package io.github.etases.edublock.rs.internal.account;
 import io.github.etases.edublock.rs.entity.Account;
 import lombok.experimental.UtilityClass;
 import org.hibernate.Session;
+import org.tinylog.Logger;
 
 import java.sql.Date;
+import java.text.Normalizer;
+import java.util.regex.Pattern;
 
 @UtilityClass
 public final class AccountUtil {
@@ -39,6 +42,35 @@ public final class AccountUtil {
                 sb.append(c);
             }
         }
-        return sb.toString();
+        String username = sb.toString();
+        username = unaccent(username);
+        return username;
+    }
+
+    private static String unaccent(String str) {
+        str = str.replaceAll("[àáạảãâầấậẩẫăằắặẳẵ]", "a");
+        str = str.replaceAll("[èéẹẻẽêềếệểễ]", "e");
+        str = str.replaceAll("[ìíịỉĩ]", "i");
+        str = str.replaceAll("[òóọỏõôồốộổỗơờớợởỡ]", "o");
+        str = str.replaceAll("[ùúụủũưừứựửữ]", "u");
+        str = str.replaceAll("[ỳýỵỷỹ]", "y");
+        str = str.replace("đ", "d");
+
+        str = str.replaceAll("[ÀÁẠẢÃÂẦẤẬẨẪĂẰẮẶẲẴ]", "A");
+        str = str.replaceAll("[ÈÉẸẺẼÊỀẾỆỂỄ]", "E");
+        str = str.replaceAll("[ÌÍỊỈĨ]", "I");
+        str = str.replaceAll("[ÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠ]", "O");
+        str = str.replaceAll("[ÙÚỤỦŨƯỪỨỰỬỮ]", "U");
+        str = str.replaceAll("[ỲÝỴỶỸ]", "Y");
+        str = str.replace("Đ", "D");
+
+        try {
+            String temp = Normalizer.normalize(str, Normalizer.Form.NFD);
+            Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+            return pattern.matcher(temp).replaceAll("").replace(" ", "-");
+        } catch (Exception ex) {
+            Logger.error(ex, "Error when unaccenting string");
+            return str;
+        }
     }
 }
